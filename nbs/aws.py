80030c552c78e66cc20bdd3f7a0a7c5da9db8f94
simonm3 <simonm3@gmail.com>
2020-09-13 15:11:40 +0100

    .

--- nbs/aws.py
+++ nbs/aws.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.5.0
+#       jupytext_version: 1.5.2
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -21,11 +21,17 @@ m = Manager()
 m.add(AWS, 2)
 search = m.get_proxy_function(google.search)
 
+for _ in range(2):
+    print(m.get_session().get("https://api.ipify.org").text)
+
 # run searches
 urls = search("trump", before="20200701", after="20200701")
 len(urls), urls[:10]
 
+# remove a single proxy
 m.remove()
 
 # stop proxies
 m.stop()
+
+
